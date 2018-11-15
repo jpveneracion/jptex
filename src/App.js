7f02b7a0434625f4d5ec09781fc6edbb0d55fbcb
John Paul Veneracion <jpveneracion@gmail.com>
2018-11-15 12:19:35 +0800

    fixed app crash

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "./style/style.css";
+import "./style/bootstrap.min.css";
+import Header from "./components/header/header";
+import TripTable from "./components/tripTable/trips";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Header />
+        <br />
+        <TripTable />
+      </React.Fragment>
     );
   }
 }
